c7ab4c22a35cdd597cd7503459f3e580252ac44d
pinpinpin <pinpinpin@odds.team>
2025-11-01 18:16:35 +0700

    Initial Commit

---
+++ my-first-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-first-app
+//
+//  Created by pinpinpin on 1/11/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
